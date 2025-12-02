92bf71a7b362c1c45db263501c6a7c2f0768f235
freyjaAI <tech@freyjafinancialgroup.net>
2025-12-02 18:07:04 -0500

    Fix indentation error for EIN field in UniversalSearchRequest

--- api/universal_search.py
+++ api/universal_search.py
@@ -15,7 +15,7 @@ class UniversalSearchRequest(BaseModel):
     entity_type: str = "person"  # person or business
     email: str = None
     address: str = None
-         str = None  # Employer Identification Number for business search
+    ein: str = None  # Employer Identification Number for business search
 
 class UniversalSearchResponse(BaseModel):
     canon_id: str
